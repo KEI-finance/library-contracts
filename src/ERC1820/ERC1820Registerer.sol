e514def5a6dea249eec5634f9a5df490b3528d6b
Kevin Upton <k_upton@live.com>
2024-02-27 00:03:08 -0600

    forge install: openzeppelin-contracts-upgradeable
    v5.0.1

--- src/ERC1820/ERC1820Registerer.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/interfaces/IERC1820Registry.sol";
-
-contract ERC1820Registerer {
-    IERC1820Registry internal constant _ERC1820_REGISTRY = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
-}
